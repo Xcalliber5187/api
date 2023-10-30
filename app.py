9f3a7acb49fd8216ea53b7b9ec3e8b78af021161
Xcalliber5187 <111370208+Xcalliber5187@users.noreply.github.com>
2023-10-30 00:00:32 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "manbearpig_MUDMAN888"
+
+
+from discord_webhook import DiscordWebhook
+
+import time
+
+
+@app.route("/hello")
+def index():
+	flash("what's your name?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	
+	address = str(request.remote_addr)
+
+
+
+
+
+	webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1168329715547918396/tInXbaiewZtPjWDSIoscBfgFfbunvddaNZ7HNNWl4nGVSZPgRqENtcXpK7xfpFO1B-TL", content=str(time.ctime())+" "+address+" "+"'"+str(request.form['name_input'])+"'")
+	webhook.execute()
+	return render_template("index.html")
